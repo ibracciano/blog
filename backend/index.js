58b94a61781f6bc020537449e93c21eaa9e61d21
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-08-12 23:34:34 +0000

    connexion à la base de donnée

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 
@@ -6,6 +10,15 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+// connection à la base de données
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+})
+
+// ecoute du port
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
