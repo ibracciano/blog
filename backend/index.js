f650a91727cc68974412b8773796a1636e8731c8
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-08-12 23:53:30 +0000

    creation de la route utilisateur

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+// import routes
+import userRoutes from './routes/user.route.js';
+
 dotenv.config();
+
 
 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// middleware pour accepter les données JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // connection à la base de données
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -23,3 +31,4 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+app.use('/api/user', userRoutes)
