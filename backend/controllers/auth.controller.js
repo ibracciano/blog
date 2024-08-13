57ce62798840703ddd7226dff66d62df8a44537c
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-08-13 02:59:23 +0000

    creation de la route sign in

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body
@@ -20,5 +21,41 @@ export const signup = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
+}
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password || email.length === 0 || password.length === 0) {
+        return res.status(400).json({ message: 'Email and password are required' })
+    }
+    try {
+        // chercher si l'utilisateur existe
+        const validateUser = await User.findOne({ email })
+        if (!validateUser) {
+            return res.status(400).json({ message: 'User not found' })
+        }
+
+        // vérifier si le mot de passe est correcte
+        const validPassword = await bcryptjs.compare(password, validateUser.password)
+        if (!validPassword) {
+            return res.status(400).json({ message: 'Invalid password' })
+        }
+
+        // generer un token d'authentification
+        const token = jwt.sign({ id: validateUser._id }, process.env.JWT_SECRET)
+
+        // ne pas ramener le mot de passe de l'utilisateur
+        const { password: pass, ...userWithoutPassword } = validateUser._doc
+
+
+        // creer un cookie
+        res.cookie('token', token, { httpOnly: true }).json(userWithoutPassword)
+
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+
 }
 
